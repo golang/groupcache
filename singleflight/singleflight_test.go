096a0f17b5669d2978abd18930c4d4ab0f3b64ff
Kevin Burke <kev@inburke.com>
2018-05-12 17:35:36 -0700

    singleflight: add OnceGroup
    OnceGroup.Do has the same semantics as Group.Do, but caches and
    returns the first computed result if called sequentially.
    
    Fixes #96.

--- singleflight/singleflight_test.go
+++ singleflight/singleflight_test.go
@@ -83,3 +83,114 @@ func TestDoDupSuppress(t *testing.T) {
 		t.Errorf("number of calls = %d; want 1", got)
 	}
 }
+
+func TestDoCalledTwice(t *testing.T) {
+	var g Group
+	c := make(chan string)
+	var calls int32
+	fn := func() (interface{}, error) {
+		atomic.AddInt32(&calls, 1)
+		return <-c, nil
+	}
+
+	const n = 10
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			v, err := g.Do("key", fn)
+			if err != nil {
+				t.Errorf("Do error: %v", err)
+			}
+			if v.(string) != "bar" {
+				t.Errorf("got %q; want %q", v, "bar")
+			}
+			wg.Done()
+		}()
+	}
+	time.Sleep(100 * time.Millisecond) // let goroutines above block
+	c <- "bar"
+	wg.Wait()
+	go func() {
+		// call one more time; fn() should get called a second time
+		v, err := g.Do("key", fn)
+		if err != nil {
+			t.Errorf("Do error: %v", err)
+		}
+		if v.(string) != "bar" {
+			t.Errorf("got %q; want %q", v, "bar")
+		}
+	}()
+	c <- "bar"
+	if got := atomic.LoadInt32(&calls); got != 2 {
+		t.Errorf("number of calls = %d; want 2", got)
+	}
+}
+
+func TestOnceDo(t *testing.T) {
+	var g OnceGroup
+	v, err := g.Do("key", func() (interface{}, error) {
+		return "bar", nil
+	})
+	if got, want := fmt.Sprintf("%v (%T)", v, v), "bar (string)"; got != want {
+		t.Errorf("Do = %v; want %v", got, want)
+	}
+	if err != nil {
+		t.Errorf("Do error = %v", err)
+	}
+}
+
+func TestOnceDoErr(t *testing.T) {
+	var g OnceGroup
+	someErr := errors.New("Some error")
+	v, err := g.Do("key", func() (interface{}, error) {
+		return nil, someErr
+	})
+	if err != someErr {
+		t.Errorf("Do error = %v; want someErr", err)
+	}
+	if v != nil {
+		t.Errorf("unexpected non-nil value %#v", v)
+	}
+}
+
+func TestOnceDoDupSuppress(t *testing.T) {
+	var g OnceGroup
+	c := make(chan string)
+	var calls int32
+	fn := func() (interface{}, error) {
+		atomic.AddInt32(&calls, 1)
+		return <-c, nil
+	}
+
+	const n = 10
+	var wg sync.WaitGroup
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			v, err := g.Do("key", fn)
+			if err != nil {
+				t.Errorf("Do error: %v", err)
+			}
+			if v.(string) != "bar" {
+				t.Errorf("got %q; want %q", v, "bar")
+			}
+			wg.Done()
+		}()
+	}
+	time.Sleep(100 * time.Millisecond) // let goroutines above block
+	c <- "bar"
+	wg.Wait()
+	// one more time after every goroutine has completed - should return the
+	// same result instantly.
+	v, err := g.Do("key", fn)
+	if err != nil {
+		t.Errorf("Do error: %v", err)
+	}
+	if v.(string) != "bar" {
+		t.Errorf("got %q; want %q", v, "bar")
+	}
+	if got := atomic.LoadInt32(&calls); got != 1 {
+		t.Errorf("number of calls = %d; want 1", got)
+	}
+}
